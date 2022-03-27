c846d3ee4104793b71cc7147d30111c4be6f0383
sebastianflint <sebastian@flint-familie.de>
2022-03-27 01:09:57 +0100

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblProperties = new System.Windows.Forms.Label();
+            this.cmbAMVersion = new System.Windows.Forms.ComboBox();
+            this.lblMatchingBuild = new System.Windows.Forms.Label();
+            this.btnOpenAamp = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 197);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(331, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(359, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load AAMP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblProperties
+            // 
+            this.lblProperties.AutoSize = true;
+            this.lblProperties.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProperties.Location = new System.Drawing.Point(19, 181);
+            this.lblProperties.Name = "lblProperties";
+            this.lblProperties.Size = new System.Drawing.Size(68, 13);
+            this.lblProperties.TabIndex = 3;
+            this.lblProperties.Text = "Properties:";
+            // 
+            // cmbAMVersion
+            // 
+            this.cmbAMVersion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAMVersion.FormattingEnabled = true;
+            this.cmbAMVersion.Location = new System.Drawing.Point(359, 208);
+            this.cmbAMVersion.Name = "cmbAMVersion";
+            this.cmbAMVersion.Size = new System.Drawing.Size(222, 21);
+            this.cmbAMVersion.TabIndex = 4;
+            // 
+            // lblMatchingBuild
+            // 
+            this.lblMatchingBuild.AutoSize = true;
+            this.lblMatchingBuild.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatchingBuild.Location = new System.Drawing.Point(356, 181);
+            this.lblMatchingBuild.Name = "lblMatchingBuild";
+            this.lblMatchingBuild.Size = new System.Drawing.Size(95, 13);
+            this.lblMatchingBuild.TabIndex = 5;
+            this.lblMatchingBuild.Text = "Matching Build:";
+            // 
+            // btnOpenAamp
+            // 
+            this.btnOpenAamp.Location = new System.Drawing.Point(489, 246);
+            this.btnOpenAamp.Name = "btnOpenAamp";
+            this.btnOpenAamp.Size = new System.Drawing.Size(92, 23);
+            this.btnOpenAamp.TabIndex = 6;
+            this.btnOpenAamp.Text = "Open Console";
+            this.btnOpenAamp.UseVisualStyleBackColor = true;
+            this.btnOpenAamp.Click += new System.EventHandler(this.btnOpenAamp_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 304);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(593, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Montserrat Hairline", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(32, 68);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(364, 35);
+            this.lblHeader.TabIndex = 10;
+            this.lblHeader.Text = "AMConfigVersionSelector";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat Hairline", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 35);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "by SVA";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources._8111415_switchcurved_switch_next_forward_arrow_icon;
+            this.pictureBox3.Location = new System.Drawing.Point(160, 9);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(109, 56);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.ivanti_logo;
+            this.pictureBox2.Location = new System.Drawing.Point(436, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(145, 69);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.SVA_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(436, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 326);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnOpenAamp);
+            this.Controls.Add(this.lblMatchingBuild);
+            this.Controls.Add(this.cmbAMVersion);
+            this.Controls.Add(this.lblProperties);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AMConfigVersionSelector";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form1_DragOver);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblProperties;
+        private System.Windows.Forms.ComboBox cmbAMVersion;
+        private System.Windows.Forms.Label lblMatchingBuild;
+        private System.Windows.Forms.Button btnOpenAamp;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AppSense.Aom;
+using EMConsole;
+using AppSense.Aom.BranchManagement;
+
+namespace WindowsFormsApp1
+{
+
+    /* Script Name  : AMConfigVersionSelector.exe
+     * Description  : The AMConfigVersionselector is a program designed to read the Ivanti Application Control Config.
+     * Author       : Sebastian Flint
+     * E-Mail       : sebastian.flint@sva.de
+     * 
+     * Included Build   Version
+     * 10.3.61.0        2019.1
+     * 10.5.268.0       2020.2
+     * 10.6.111.0       2020.3
+     * 10.7.17.0        2021.1
+     * 10.8.61.0        2021.3
+     * 
+     */
+    public partial class Form1 : Form
+    {
+        string[] AMVersions = { "2019.1", "2020.2", "2020.3", "2021.1", "2021.3" };
+        string[] AMBuilds = { "10.3.61.0", "10.5.268.0", "10.6.111.0", "10.7.17.0", "10.8.61.0" };
+        string[] AMCombined = { "2019.1 (10.3.61.0)", "2020.2 (10.5.268.0)", "2020.3 (10.6.111.0)", "2021.1 (10.7.17.0)", "2021.3 (10.8.61.0)" };
+        string AampPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            foreach (string version in AMCombined)
+             {
+                 cmbAMVersion.Items.Add(version);
+             }
+             
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "aamp file (*.aamp)|*.aamp|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+
+             AampPath = openFileDialog1.FileName;
+
+            try
+            {
+                GetAAMPProperties(AampPath);
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void GetAAMPProperties(string path)
+
+        {
+            cmbAMVersion.SelectedItem = null;
+
+            Common.Deployment.PackageManager manager = new Common.Deployment.PackageManager();
+
+            manager.ReadManifest(path);
+            string configname = manager.Manifest.ConfigName.ToString();
+            string creatorversion = manager.Manifest.CreatorVersion.ToString();
+            string marketingversion = manager.Manifest.MarketingVersion.ToString();
+            string productname = manager.Manifest.ProductName.ToString();
+
+            richTextBox1.Clear();
+            richTextBox1.AppendText("Configpath: ");
+            richTextBox1.AppendText(path);
+            richTextBox1.AppendText("\n");
+            richTextBox1.AppendText("Configname: ");
+            richTextBox1.AppendText(configname);
+            richTextBox1.AppendText("\n");
+            richTextBox1.AppendText("Creator Version: ");
+            richTextBox1.AppendText(creatorversion);
+            richTextBox1.AppendText("\n");
+            richTextBox1.AppendText("Version: ");
+            richTextBox1.AppendText(marketingversion);
+            richTextBox1.AppendText("\n");
+            richTextBox1.AppendText("Productname: ");
+            richTextBox1.AppendText(productname);
+
+            int i = 0;
+
+            foreach (string version in AMBuilds)
+            {
+                if (creatorversion == version)
+                {
+                    cmbAMVersion.SelectedIndex = i;
+                }
+                i++;
+            }
+        }
+
+        private void Form1_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[]; 
+            if (files != null && files.Any())
+                try
+                {
+                    GetAAMPProperties(files.First());
+                }
+                catch
+                {
+
+                }
+                
+        }
+
+        private void btnOpenAamp_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string ProgramPath = AMVersions[cmbAMVersion.SelectedIndex];
+
+                Process ExternalProcess = new Process();
+                ExternalProcess.StartInfo.FileName = @"C:\tmp\" + ProgramPath + @"\AMConsole.exe";
+               if(AampPath == "")
+                {
+                    
+                }
+                else
+                {
+                    ExternalProcess.StartInfo.Arguments = AampPath;
+                }
+                ExternalProcess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+                ExternalProcess.Start();
+
+                toolStripStatusLabel1.Text = "Starting Console Version " + ProgramPath;
+            }
+            catch
+            {
+                MessageBox.Show("Could not start AMConsole.exe");
+            }
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.sva.de");
+        }
+
+    }
+}
